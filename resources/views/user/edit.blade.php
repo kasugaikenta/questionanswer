7694a617ceda08f5b15fe6d6f47bb394b9d2980a
EC2 Default User <ec2-user@ip-172-31-45-195.ap-northeast-1.compute.internal>
2020-05-22 06:23:48 +0000

    ユーザ情報編集画面の日本語対応

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -21,7 +21,7 @@
         </div>
       </div>
       <div class="form-group row">
-        <label for="password" class="col-md-3 col-form-label text-md-right">新しい{{ __('Password') }}</label>
+        <label for="password" class="col-md-3 col-form-label text-md-right">新しい{{ __('パスワード') }}</label>
 
         <div class="col-md-6">
             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  value="{{old('user_password')}}">
@@ -35,7 +35,7 @@
       </div>
 
       <div class="form-group row">
-        <label for="password-confirm" class="col-md-3 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+        <label for="password-confirm" class="col-md-3 col-form-label text-md-right">{{ __('パスワードの確認') }}</label>
 
         <div class="col-md-6">
             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" >
